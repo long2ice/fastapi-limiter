e4f28d2ff18a345ce2fe69730a2fa9716efec522
ErikASD <khaoticcarmae@gmail.com>
2021-04-28 16:10:34 -0700

    Rate Limit Bypass with X-Forwarded-For header

--- fastapi_limiter/__init__.py
+++ fastapi_limiter/__init__.py
@@ -9,9 +9,11 @@ from starlette.status import HTTP_429_TOO_MANY_REQUESTS
 
 
 async def default_identifier(request: Request):
+#This code allows any requester to override the ratekey by sending the header X-Forwarded-For with any data and just use a random generator to never trip the ratelimiter
     forwarded = request.headers.get("X-Forwarded-For")
     if forwarded:
         return forwarded.split(",")[0]
+#################################################################
     return request.client.host + ":" + request.scope["path"]
 
 
