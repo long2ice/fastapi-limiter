2d65fd13cae4907d13538a51e2045b488318f162
tkrtmy <tkrtmy1333@gmail.com>
2023-12-24 11:05:23 +0900

    Use Optional

--- fastapi_limiter/__init__.py
+++ fastapi_limiter/__init__.py
@@ -1,5 +1,5 @@
 from math import ceil
-from typing import Callable, Union
+from typing import Callable, Optional, Union
 
 from fastapi import HTTPException
 from starlette.requests import Request
@@ -46,11 +46,11 @@ async def ws_default_callback(ws: WebSocket, pexpire: int):
 
 class FastAPILimiter:
     redis = None
-    prefix: str = None
-    lua_sha: str = None
-    identifier: Callable = None
-    http_callback: Callable = None
-    ws_callback: Callable = None
+    prefix: Optional[str] = None
+    lua_sha: Optional[str] = None
+    identifier: Optional[Callable] = None
+    http_callback: Optional[Callable] = None
+    ws_callback: Optional[Callable] = None
     lua_script = """local key = KEYS[1]
 local limit = tonumber(ARGV[1])
 local expire_time = ARGV[2]
